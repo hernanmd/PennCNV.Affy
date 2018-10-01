cd7ed2cdeb18d974323ffff13196d7e191250f29
hernanmd <hernan.morales@gmail.com>
2018-10-01 14:10:59 -0300

    Steps 1 & 2 completed

--- run_penncnv_AxiomGT1.sh
+++ run_penncnv_AxiomGT1.sh
@@ -11,43 +11,43 @@
 # Input parameters
 ###########################################
 
+set -x
+
 # Default directory for AAS: 'c:\Users\Public\Documents\AxiomAnalysisSuite\Library\Axiom_GW_Bos_SNP_1.r3'
-library_dir='/c/Users/Public/Documents/AxiomAnalysisSuite/Library/'
-xml_file=$library_dir'Axiom_GW_Bos_SNP_1_96orMore_Step1.r3.apt-probeset-genotype.AxiomGT1.xml'
-list_file='cel_files_list.txt'
-pfb_file='.pfb'
-results_dir='output'
+cel_files_dir="004"
+library_dir="/c/Users/Public/Documents/AxiomAnalysisSuite/Library/Axiom_GW_Bos_SNP_1.r3/"
+xml_file=${library_dir}'Axiom_GW_Bos_SNP_1_96orMore_Step1.r3.apt-probeset-genotype.AxiomGT1.xml'
+#xml_file=${library_dir}'Axiom_GW_Bos_SNP_1_96orMore_Step1.r3.apt-probeset-genotype.AxiomGT1.xml'
+list_file="cel_files_list.txt"
+probeset_file=${library_dir}"Axiom_GW_Bos_SNP_1.r3.cdf"
+pfb_file=".pfb"
+results_dir="output"
+
+[ -d $library_dir ] ||  { echo "ERROR: Library directory not found"; exit 1; }
 
 #
 # 1. Make a file containing a list of all CEL files + location
 #
 
-ls -d $PWD/*.CEL > $list_file
-sed -i '1s/^/cel_files/' $list_file
+echo "cel_files" > $list_file
+ls -d $cel_files_dir/*.CEL >> $list_file
 
 #
 # 2. Generate genotyping calls from CEL files
 #
 
-apt-probeset-genotype \
-	--analysis-files-path ${library_dir} \
-	--xml-file ${xml_file} \
-	--out-dir ${results_dir} \
-	--cel-files ${$list_file}	
-	
-	#
+# apt-probeset-genotype -v 2 --log-file "apt-probeset-genotype.log" --analysis-files-path ${library_dir} --xml-file ${xml_file} --out-dir ${results_dir} --cel-files ${list_file}
+# apt-genotype-axiom --log-file "apt-genotype-axiom.log" --analysis-files-path ${library_dir} --arg-file ${xml_file} --out-dir ${results_dir} --cel-files ${list_file}
+
+
+#
 # 3. Allele-specific signal extraction from CEL files
 #
 # The below will extract signal intensity values for PM probes in all the CEL files specified in the listfile. The values are then quantile normalized,  median polished, generating A & B signal intensity values for each SNP. The filehapmap.quant-norm.normalization-target.txt is is the reference quantile distribution from the HapMap3 project.
 
 # Genome-wide 6.0 array
-apt-probeset-summarize \
-	--cdf-file $probeset_file \
-	--analysis quant-norm.sketch=50000,pm-only,med-polish,expr.genotype=true \
-	--target-sketch $sketch_file \
-	--out-dir $results_dir \
-	--cel-files $list_file
-	
+apt-probeset-summarize --analysis "quant-norm.sketch=50000,pm-only,med-polish,expr.genotype=true" --target-sketch $sketch_file --out-dir $results_dir --cel-files $list_file -v 2 --cdf-file $probeset_file
+
 #
 # 4. create sex file from birdseed report
 #
@@ -59,7 +59,7 @@ fgrep male ${results_dir}birdseed.report.txt | cut -f 1,2 > ${results_dir}gender
 #
 # 5. Generate canonical genotype clustering file
 #
-# The following step will generate canonical genotype clusters. 
+# The following step will generate canonical genotype clusters.
 # file_sex = two-column file that annotates the sex information for each CEL file
 # 10918.CEL       male
 # 10924.CEL       male
@@ -84,6 +84,6 @@ generate_affy_geno_cluster.pl \
 
 normalize_affy_geno_cluster.pl \
 	${genoclust} ${qnpmpes} \
-	-locfile ${pfb_file}
+	-locfile ${pfb_file} \
 	-out ${results_dir}.lrr_baf.txt
 
