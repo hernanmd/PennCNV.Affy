4d9454b49ff9f0ff97c78c59239b7050741667a7
hernanmd <hernan.morales@gmail.com>
2018-10-08 14:15:07 -0300

    Adding Detect CNV

--- run_penncnv_AxiomGT1.sh
+++ run_penncnv_AxiomGT1.sh
@@ -19,11 +19,12 @@
 # Uncomment to debug this script
 #set -x
 
+prj_prefix=$1
 penncnv_dir='/c/git_projects/PennCNV/affy/bin/'
 PATH=$PATH:$penncnv_dir:/c/Program\ Files/Thermo\ Fisher\ Scientific/Analysis\ Power\ Tools/APT-2.10.0/bin/
 
 # Default directory for AAS: 'c:\Users\Public\Documents\AxiomAnalysisSuite\Library\Axiom_GW_Bos_SNP_1.r3'
-cel_files_dir='001/'
+cel_files_dir='004/'
 cel_list_file=$(pwd)'cel_files_list.txt'
 results_dir="output/"
 pfb_file="AxiomGT1.pfb"
@@ -44,9 +45,10 @@ computed_gender_file=${results_dir}'gender_computed.txt'
 apt_geno=$(type -p apt-genotype-axiom)
 apt_summarize=$(type -p apt-probeset-summarize)
 
-#·
-# PennCNV params
-#
+##############################################
+# PennCNV general parameters
+##############################################
+
 if [ -f "/c/Perl64/bin/perl" ]; then
 	perlexec="/c/Perl64/bin/perl"
 else
@@ -54,6 +56,36 @@ else
 fi
 gen_cluster_exec=$(type -p generate_affy_geno_cluster.pl)
 norm_cluster_exec=$(type -p normalize_affy_geno_cluster.pl)
+
+
+##############################################
+# PennCNV: Detect CNV parameters
+##############################################
+
+# Call CNVs containing more than or equal to 3 SNPs
+minsnp=3
+# Last chromosome nr
+lastchr=31
+
+############ For main HMM file
+hmm1="/usr/local/src/PennCNV-1.0.4/lib/hhall.hmm"
+log_file1=hmm1.minsnp_"$minsnp".log
+raw_file1=hmm1.minsnp_"$minsnp".rawcnv
+qc_log_file1=hmm1_minsnp_"$minsnp".log
+qclrrsd1=0.3
+qc_passout1=hmm1_minsnp_"$minsnp".qcpass
+qc_sumout1=hmm1_minsnp_"$minsnp".qcsum
+qc_goodcnv1=hmm1_minsnp_"$minsnp".goodcnv
+
+##############################################################
+# Output files:
+##############################################################
+
+pfb_file=${prj_prefix}".pfb"
+gc_file_model=${prj_prefix}".gcmodel"
+signal_outdir="PennCNV_run1/"
+signal_outsuffix=".txt"
+signal_file_list="signal_fltr_files.txt"
 
 ###########################################
 # Sanity checks
@@ -105,6 +137,33 @@ fgrep male ${ax_report_file} | cut -f 1,2 > ${computed_gender_file}
 ######################################################################################
 
 
+
+##############################################
+# Compile PFB
+##############################################
+
+# No .pfb files? Use compile_pfb.pl
+# ls='ls -lkF --color=auto'
+echo "Creating signal file list"
+#unalias ls
+signal_files=$(ls -1 $signal_output_dir)
+echo "Created signal file list: $signal_files"
+
+# The output is a new file with directory/signal_file_name in each line
+for f in $signal_files; do
+	echo $signal_output_dir/$f >> $signal_file_list
+done
+
+# SNP_Map.txt from SNP_Map.zip in the Illumina raw files
+# The output is a .pfb file
+echo "About compiling PFB..."
+compile_pfb.pl \
+	--listfile $signal_file_list \
+	--snpposfile $snp_map \
+	--output $pfb_file
+echo "done compile PFB"
+
+
 #
 # 5. Generate canonical genotype clustering file
 #
@@ -113,11 +172,11 @@ fgrep male ${ax_report_file} | cut -f 1,2 > ${computed_gender_file}
 # 10918.CEL       male
 # 11321_2.CEL     female
 
-$perlexec $gen_cluster_exec ${ax_calls_file} ${ax_confs_file} ${ax_qnpmpes_file} -locfile ${pfb_file} -sexfile ${computed_gender_file} -out ${genoclust}
+# $perlexec $gen_cluster_exec ${ax_calls_file} ${ax_confs_file} ${ax_qnpmpes_file} -locfile ${pfb_file} -sexfile ${computed_gender_file} -out ${genoclust}
 
 #
 # 6. Calculate LRR and BAF
 #
 # Finally, we can extract allele-specific signal intensity measures to calculate the Log R Ratio (LRR) values and the B Allele Frequency (BAF) values for each marker in each individual. The cluster file gw6.genocluster is a file which specifies the chromosome position of each SNP or CN probe.
 
-$perlexec $norm_cluster_exec ${genoclust} ${ax_qnpmpes_file} -locfile ${pfb_file} -out ${results_dir}.lrr_baf.txt
+# $perlexec $norm_cluster_exec ${genoclust} ${ax_qnpmpes_file} -locfile ${pfb_file} -out ${results_dir}.lrr_baf.txt
